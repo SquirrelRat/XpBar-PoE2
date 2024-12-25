03274d26daa04c7e8b9530348733571b2d6b7bdd
SquirrelRat <37273964+SquirrelRat@users.noreply.github.com>
2024-12-25 17:13:54 -0600

    Added Text Scale + CleanUp Code

--- Settings.cs
+++ Settings.cs
@@ -7,16 +7,19 @@ namespace XPBar
 {
     public class Settings : ISettings
     {
-	[Menu("Enabled")]
+        [Menu("Enabled")]
         public ToggleNode Enable { get; set; } = new ToggleNode(true);
-		
-	[Menu("Text Color")]
-	public ColorNode TextColor { get; set; } = new ColorNode(Color.White);
-	
-	[Menu("X Position")]
-    public RangeNode<int> XPos { get; set; } = new RangeNode<int>(950, 0, 2160);
-	
-	[Menu("Y Position")]
-    public RangeNode<int> YPos { get; set; } = new RangeNode<int>(1065, 0, 3840);
+        
+        [Menu("Text Color")]
+        public ColorNode TextColor { get; set; } = new ColorNode(Color.White);
+        
+        [Menu("X Position")]
+        public RangeNode<int> XPos { get; set; } = new RangeNode<int>(950, 0, 2160);
+        
+        [Menu("Y Position")]
+        public RangeNode<int> YPos { get; set; } = new RangeNode<int>(1065, 0, 3840);
+        
+        [Menu("Text Scale")]
+        public RangeNode<float> TextSize { get; set; } = new RangeNode<float>(1, 0.5f, 5);
     }
 }
